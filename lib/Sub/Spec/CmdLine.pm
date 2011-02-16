ca65e5e8513bbbcfe963838a1b055a04367ae586
Steven Haryanto (on PC) <stevenharyanto@gmail.com>
2011-02-16 18:22:35 +0700

    implement per-subcommand help

--- lib/Sub/Spec/CmdLine.pm
+++ lib/Sub/Spec/CmdLine.pm
@@ -456,7 +456,15 @@ _
 
     # handle per-command --help
     if ($subcmd && $ARGV[0] && $ARGV[0] =~ /^(--help|-h|-\?)$/) {
-        print gen_usage($spec, {cmd=>"$cmd $subcmdname"});
+        if ($subcmd->{help}) {
+            if (ref($subcmd->{help}) eq 'CODE') {
+                print $subcmd->{help}->();
+            } else {
+                print $subcmd->{help};
+            }
+        } else {
+            print gen_usage($spec, {cmd=>"$cmd $subcmdname"});
+        }
         if ($exit) { exit 0 } else { return 0 }
     }
 
