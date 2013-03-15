00aed87e505fb6a097f3afbe7173b1975a691684
Steven Haryanto (on PC) <stevenharyanto@gmail.com>
2013-03-15 13:22:55 +0700

    Allow setting Perinci::Access options via pa_options (undocumented yet)

--- lib/Perinci/CmdLine.pm
+++ lib/Perinci/CmdLine.pm
@@ -57,6 +57,7 @@ has undo_dir => (
 has format => (is => 'rw', default=>sub{'text'});
 has format_options => (is => 'rw');
 has format_options_set => (is => 'rw'); # bool
+has pa_options => (is => 'rw');
 has _pa => (
     is => 'rw',
     lazy => 1,
@@ -64,7 +65,7 @@ has _pa => (
         my $self = shift;
 
         require Perinci::Access;
-        my %args;
+        my %args = %{$self->pa_options // {}};
         if ($self->undo) {
             require Perinci::Access::InProcess;
             my $pai = Perinci::Access::InProcess->new(
@@ -1095,7 +1096,7 @@ sub run {
 1;
 # ABSTRACT: Rinci/Riap-based command-line application framework
 
-=for Pod::Coverage ^(BUILD|run_.+|doc_.+|before_.+|after_.+|format_and_display_result|gen_common_opts|get_subcommand|list_subcommands|parse_common_opts|parse_subcommand_opts|format_options|format_options_set)$
+=for Pod::Coverage ^(BUILD|run_.+|doc_.+|before_.+|after_.+|format_and_display_result|gen_common_opts|get_subcommand|list_subcommands|parse_common_opts|parse_subcommand_opts|format_options|format_options_set|pa_options)$
 
 =head1 SYNOPSIS
 
